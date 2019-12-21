24a0e6f450dc1cbe927a0c828e8143b0bc26ed70
Petr Kuznetsov <vold-world@yandex.ru>
2019-12-21 14:12:31 +0300

    *Added check standart fields ftom regular expressions. *Added component for easy create anchors.

---
+++ Editor/Anchors/AnchorsFromCurrentScreenEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using RanterTools.UI;
+namespace RanterTools.UI.Editor
+{
+
+    [CustomEditor(typeof(AnchorsFromCurrentScreen))]
+    public class AnchorsFromCurrentScreenEditor : UnityEditor.Editor
+    {
+        #region Events
+
+        #endregion Events
+
+        #region Global State
+
+        #endregion Global State
+
+        #region Global Methods
+
+        #endregion Global Methods
+
+        #region Parameters
+
+        #endregion Parameters
+
+        #region State
+        AnchorsFromCurrentScreen instance;
+        #endregion State
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Unity
+        public override void OnInspectorGUI()
+        {
+            instance = target as AnchorsFromCurrentScreen;
+            if (GUILayout.Button("Apply anchors"))
+            {
+                instance.ApplyAnchors();
+            }
+            if (GUILayout.Button("Revert anchors"))
+            {
+                instance.RevertAnchors();
+            }
+        }
+        #endregion Unity
+    }
+}
---
+++ Runtime/Debug/ToolsDebug.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RanterTools.UI
+{
+
+
+    public class ToolsDebug
+    {
+        public static void Log(object log)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.Log(log);
+#endif
+        }
+        public static void Log(object log, Object context)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.Log(log, context);
+#endif
+        }
+
+        public static void LogError(object log)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.LogError(log);
+#endif
+        }
+        public static void LogError(object log, Object context)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.LogError(log, context);
+#endif
+        }
+
+        public static void LogWarning(object log)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.Log(log);
+#endif
+        }
+        public static void LogWarning(object log, Object context)
+        {
+#if RANTER_TOOLS_DEBUG_NETWORKING
+            Debug.Log(log, context);
+#endif
+        }
+    }
+}
