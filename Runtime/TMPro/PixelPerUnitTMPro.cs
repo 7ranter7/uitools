61184d3834037c40b9be46a6cdfa10f3edb8851b
Petr Kuznetsov <vold-world@yandex.ru>
2020-04-23 13:47:05 +0300

    *Added PixelPerUnity for TMPro font size.

---
+++ Runtime/TMPro/PixelPerUnitTMPro.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+namespace RanterTools.UI
+{
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class PixelPerUnitTMPro : MonoBehaviour
+    {
+
+        #region Global State
+#if UNITY_EDITOR
+        static List<PixelPerUnitTMPro> All = new List<PixelPerUnitTMPro>();
+#endif
+        #endregion Global State
+
+        #region Global Methods
+#if UNITY_EDITOR
+        [MenuItem("RanterTools/UI/DPI/ForceUpdate")]
+        static void UpdateAll()
+        {
+            foreach (var p in All)
+            {
+                p.UpdateParameters();
+            }
+        }
+#endif
+        #endregion Global Methods
+
+
+        #region Parameters
+        [SerializeField]
+        float fontSize = -1;
+        #endregion Parameters
+
+        #region State
+        bool cacheTried = false;
+        TextMeshProUGUI textMeshProUGUI;
+        TextMeshProUGUI TextMeshProUGUI
+        {
+            get
+            {
+                if (!cacheTried)
+                {
+                    textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+                }
+                return textMeshProUGUI;
+            }
+        }
+#if UNITY_EDITOR
+        float width;
+        float oldFont;
+#endif
+        float font;
+        #endregion State
+
+        #region Methods
+        void UpdateParameters()
+        {
+            font = fontSize * Screen.width / 1080.0f;
+            TextMeshProUGUI.fontSize = font;
+        }
+        #endregion Methods
+
+        #region Unity
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            TextMeshProUGUI.enableAutoSizing = false;
+#if UNITY_EDITOR
+            All.Add(this);
+#endif
+#if UNITY_EDITOR
+            if (fontSize == -1)
+                fontSize = TextMeshProUGUI.fontSize;
+            if (width != Screen.width || oldFont != fontSize)
+            {
+                width = Screen.width;
+                oldFont = font;
+#endif
+                UpdateParameters();
+#if UNITY_EDITOR
+            }
+#endif
+        }
+#if UNITY_EDITOR
+        /// <summary>
+        /// This function is called when the object becomes enabled and active.
+        /// </summary>
+        void OnEnable()
+        {
+            //if (fontSize == -1)
+            fontSize = TextMeshProUGUI.fontSize;
+            TextMeshProUGUI.enableAutoSizing = false;
+            if (width != Screen.width || oldFont != fontSize)
+            {
+                width = Screen.width;
+                oldFont = font;
+                UpdateParameters();
+            }
+        }
+#endif
+
+        /// <summary>
+        /// This function is called when the MonoBehaviour will be destroyed.
+        /// </summary>
+        void OnDestroy()
+        {
+#if UNITY_EDITOR
+            All.Remove(this);
+#endif
+        }
+#if UNITY_EDITOR
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+            if (width != Screen.width || oldFont != fontSize)
+            {
+                UpdateParameters();
+                width = Screen.width;
+                oldFont = font;
+            }
+        }
+#endif
+        #endregion Unity
+    }
+}
